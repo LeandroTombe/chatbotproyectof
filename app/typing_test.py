7587776a2415623340d10d14b2f5433116921d58
Leandro Tombetta <leandro.tombetta87@gmail.com>
2026-02-10 11:15:22 -0300

    first code project

---
+++ app/typing_test.py
@@ -0,0 +1,10 @@
+def sumar(a: float, b: float) -> float:
+    return a + b
+
+
+#Ejemplo de uso
+
+if __name__ == "__main__":
+
+    resultado = sumar(3.5, 2.5)
+    print(f"El resultado de la suma es: {resultado}")
